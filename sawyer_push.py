ffcff1c400867e5ba976bf7b7cbbc116252e3026
Yuke Zhu <zyk0902@gmail.com>
2017-10-30 23:54:28 -0700

    update sawyer reaching

--- sawyer_push.py
+++ sawyer_push.py
@@ -18,40 +18,25 @@ viewer = MjViewer(sim)
 viewer.vopt.geomgroup[0] = 0
 viewer.vopt.geomgroup[1] = 1
 
-# self.cam.azimuth = 179.7749999999999
-# self.cam.distance = 3.825077470729921
-# self.cam.elevation = -21.824999999999992
-# self.cam.lookat = array([ 0.09691817,  0.00164106, -0.30996464])
-
 viewer.cam.fixedcamid = 0
 viewer.cam.type = const.CAMERA_FIXED
-# viewer.cam.azimuth = 179.7749999999999
-# viewer.cam.distance = 3.825077470729921
-# viewer.cam.elevation = -21.824999999999992
-# viewer.cam.lookat[:][0] = 0.09691817
-# viewer.cam.lookat[:][1] = 0.00164106
-# viewer.cam.lookat[:][2] = -0.30996464
+viewer.cam.azimuth = 179.7749999999999
+viewer.cam.distance = 3.825077470729921
+viewer.cam.elevation = -21.824999999999992
+viewer.cam.lookat[:][0] = 0.09691817
+viewer.cam.lookat[:][1] = 0.00164106
+viewer.cam.lookat[:][2] = -0.30996464
 
 sim_state = sim.get_state()
+# viewer.render()
 
+# Note: sim.data.ctrl contains the actuator values that we can control
 while True:
-    viewer.render()
+    sim.set_state(sim_state)
 
-# # Note: sim.data.ctrl contains the actuator values that we can control
-# while True:
-#     sim.set_state(sim_state)
-#
-#     for i in range(10000):
-#         # if i < 150:
-#         #     sim.data.ctrl[:] = [float(i) / 1000 for _ in range(7)]
-#         # else:
-#         #     sim.data.ctrl[:] = [1.0 for _ in range(7)]
-#         # sim.data.ctrl[:] = np.random.rand(7) * 3
-#         # print(sim.data.ctrl[:])
-#         # print(i)
-#
-#         sim.step()
-#         viewer.render()
-#
-#     if os.getenv('TESTING') is not None:
-#         break
+    for i in range(500):
+        sim.data.ctrl[:] = np.random.rand(7) * 3
+        print(sim.data.ctrl[:])
+
+        sim.step()
+        viewer.render()
