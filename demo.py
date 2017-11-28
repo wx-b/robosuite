2dadd0180bd3f9406ab72bc83ae2ad8770255342
Yuke Zhu <zyk0902@gmail.com>
2017-11-28 13:55:31 -0800

    play with demo

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
 import time
 from MujocoManip import SawyerStackEnv
 import numpy as np
+
+
 env = SawyerStackEnv()
 while True:
 	env._reset()
 	env._render()
-	import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
 	for i in range(1000):
 		env._step(np.random.randn(8) * 100)
 		env._render()
