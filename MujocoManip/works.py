ac980c17d13058466fc8843e3422262b918117bf
amandlek <ajay.mandlekar@gmail.com>
2018-03-08 17:30:09 -0800

    updated small test script

--- MujocoManip/works.py
+++ MujocoManip/works.py
@@ -1,27 +1,48 @@
-import MujocoManip as MM
-import numpy as np
+# import MujocoManip as MM
+# import numpy as np
 
-env = MM.make("SawyerStackEnv", display=True, use_eef_ctrl=True)
+# env = MM.make("SawyerStackEnv", display=True, use_eef_ctrl=True)
+# obs = env.reset()
+# dof = env.dof()
+# print('action space', env.action_space)
+# print('Obs: {}'.format(len(obs)))
+# print('DOF: {}'.format(dof))
+# # env.render()
+
+# while True:
+#     obs = env.reset()
+
+#     for i in range(20000):
+#         action = 0.01 * np.random.randn(9)
+#         action[3:7] = [0., 1., 0., 0.] 
+#         obs, reward, done, info = env.step(action)
+#         # env.render()
+#         A = env.viewer.render_frame()
+#         if i % 100 == 0:
+#             print(i)
+#         if done:
+#             print('done: {}'.format(reward))
+#             break
+
+from dm_control import suite
+import numpy as np
+import time
+
+env = suite.load("cartpole", "swingup")
 obs = env.reset()
-dof = env.dof()
-print('action space', env.action_space)
-print('Obs: {}'.format(len(obs)))
-print('DOF: {}'.format(dof))
-# env.render()
+action_spec = env.action_spec()
 
 while True:
     obs = env.reset()
 
-    for i in range(20000):
-        action = 0.01 * np.random.randn(9)
-        action[3:7] = [0., 1., 0., 0.] 
-        obs, reward, done, info = env.step(action)
-        # env.render()
-        A = env.viewer.render_frame()
-        if i % 100 == 0:
-            print(i)
-        if done:
-            print('done: {}'.format(reward))
-            break
+    time_step = env.reset()
+    while not time_step.last():
+        action = np.random.uniform(action_spec.minimum,
+                                 action_spec.maximum,
+                                 size=action_spec.shape)
+        time_step = env.step(action)
+        t = time.time()
+        A = env.physics.render(480, 480)
+        print("took {} sec".format(time.time() - t))
 
 
