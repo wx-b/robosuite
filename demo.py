7aac31b5b96d7d07b0d4b3b86e4341c97912ab09
Yuke Zhu <zyk0902@gmail.com>
2018-04-11 20:40:40 -0700

    fixing demo.py

--- demo.py
+++ demo.py
@@ -1,15 +1,13 @@
 import time
-from MujocoManip import SawyerStackEnv
+from MujocoManip import make
 import numpy as np
 
 
-env = SawyerStackEnv(gripper=None)
+env = make("SawyerStackEnv", display=True, ignore_done=True)
+
 while True:
-	env._reset()
-	env._render()
-    # import pdb; pdb.set_trace()
+	env.reset()
+	env.render()
 	for i in range(1000):
-		env._step(np.random.randn(8) * 100)
-		env._render()
-		joint_vel = [env.sim.data.qvel[x] for x in env._ref_joint_vel_indexes]
-		print(joint_vel)
+		env.step(np.random.randn(9) * 100)
+		env.render()
