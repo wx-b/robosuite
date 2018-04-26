6b2da77151db372575d78cc1332cc7a47c8c2ae5
amandlek <ajay.mandlekar@gmail.com>
2018-04-26 10:48:24 -0700

    added helpful code for rotating cameras, and added first person camera for real

---
+++ MujocoManip/miscellaneous/camera_test.py
@@ -0,0 +1,20 @@
+import MujocoManip as MM
+import MujocoManip.miscellaneous.utils as U
+import numpy as np
+
+def rotate_and_return_quat(rot, rot_angle, direction):
+    """
+    Takes a rotation matrix, an angle in degrees and a direction, and 
+    returns a quaternion after applying a post-rotation.
+    """
+    rad = np.pi * rot_angle / 180.0
+    R = U.rotation_matrix(rad, direction, point=None)
+    mat = rot.dot(R[:3, :3])
+    return U.mat2quat(mat)
+
+if __name__ == "__main__":
+
+    base_rot = np.array([[0., 0., -1.], [1., 0., 0.], [0., -1., 0.]])
+
+    quat = rotate_and_return_quat(base_rot, 30., [0., 1., 0.])
+    print("\"{} {} {} {}\"".format(*quat))
--- demo.py
+++ demo.py
@@ -1,14 +1,16 @@
 import time
 from MujocoManip import make
 import numpy as np
+from IPython import embed
 
 # env = make("SawyerLiftEnv", ignore_done=True, use_camera_obs=False)
 # env = make("SawyerStackEnv", ignore_done=True, use_camera_obs=False)
 env = make("ApcEnv", ignore_done=True, use_camera_obs=False)
 
 while True:
-	env.reset()
-	# env.render()
-	for i in range(100000):
-		env.step(np.random.randn(9) * 100)
-		env.render()
+    env.reset()
+    env.render()
+    env.viewer.set_camera(3)
+    for i in range(100000):
+        env.step(np.random.randn(9))
+        env.render()
