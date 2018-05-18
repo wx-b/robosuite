51c3a78ce9b04f17457d59873836c0939d3c729a
anchit <anchit.post@gmail.com>
2018-05-17 17:03:32 -0700

    Minor fixes

--- demo.py
+++ demo.py
@@ -1,12 +1,6 @@
 import time
 from MujocoManip import make
 import numpy as np
-from IPython import embed
-
-
-# env = make("SawyerLiftEnv", ignore_done=True, use_camera_obs=False)
-
-env = make("SawyerBinsEnv", ignore_done=True, use_camera_obs=False)
 
 # env for training
 # env = make("SawyerLiftEnv",
@@ -19,12 +13,11 @@ env = make("SawyerBinsEnv", ignore_done=True, use_camera_obs=False)
 #             use_object_obs=False,
 #             reward_shaping=True)
 
+env = make("SawyerStackEnv", ignore_done=True, use_camera_obs=False)
 
 while True:
     env.reset()
     env.render()
-    env.viewer.set_camera(3)
-    time.sleep(1)
     for i in range(100000):
-        env.step(np.random.randn(9))
+        env.step(np.random.randn(9) * 100)
         env.render()
