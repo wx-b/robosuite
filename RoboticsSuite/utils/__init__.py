b13be4717add29e5c5a5794ca561d4f99c9f13b7
Yuke Zhu <zyk0902@gmail.com>
2018-08-13 23:27:03 -0700

    fix mujoco_py_renderer import

--- RoboticsSuite/utils/__init__.py
+++ RoboticsSuite/utils/__init__.py
@@ -2,3 +2,4 @@ from RoboticsSuite.utils.error import *
 from RoboticsSuite.utils.misc_utils import *
 from RoboticsSuite.utils.mocap_utils import *
 from RoboticsSuite.utils.transform_utils import *
+from RoboticsSuite.utils.mujoco_py_renderer import MujocoPyRenderer
