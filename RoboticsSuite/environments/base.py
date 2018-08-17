b4f53a1e4eaabcf7d26ba840d5383d560308b939
Yuke Zhu <zyk0902@gmail.com>
2018-08-17 00:50:18 -0700

    finish refactoring base.py

--- RoboticsSuite/environments/base.py
+++ RoboticsSuite/environments/base.py
@@ -1,9 +1,8 @@
-import numpy as np
-from RoboticsSuite.utils import SimulationError, XMLError, MujocoPyRenderer
-from mujoco_py import MjSim, MjRenderContextOffscreen
 from collections import OrderedDict
-import glfw
-from mujoco_py import load_model_from_path, load_model_from_xml
+from mujoco_py import MjSim, MjRenderContextOffscreen
+from mujoco_py import load_model_from_xml
+
+from RoboticsSuite.utils import SimulationError, XMLError, MujocoPyRenderer
 
 REGISTERED_ENVS = {}
 
@@ -13,9 +12,7 @@ def register_env(target_class):
 
 
 def make(env_name, *args, **kwargs):
-    """
-        Try to get the equivalent functionality of gym.make in a sloppy way.
-    """
+    """Try to get the equivalent functionality of gym.make in a sloppy way."""
     if env_name not in REGISTERED_ENVS:
         raise Exception(
             "Environment {} not found. Make sure it is a registered environment among: {}".format(
@@ -26,49 +23,46 @@ def make(env_name, *args, **kwargs):
 
 
 class EnvMeta(type):
+    """Metaclass for registering environments"""
     def __new__(meta, name, bases, class_dict):
         cls = super().__new__(meta, name, bases, class_dict)
         register_env(cls)
         return cls
 
 
-class MujocoEnv(object, metaclass=EnvMeta):
+class MujocoEnv(metaclass=EnvMeta):
     def __init__(
-        self,
-        has_renderer=True,
-        render_collision_mesh=False,
-        render_visual_mesh=True,
-        control_freq=100,
-        horizon=500,
-        ignore_done=False,
-        use_camera_obs=False,
-        camera_name=None,
-        camera_height=256,
-        camera_width=256,
-        camera_depth=False,
-        demo_config=None,
-        has_offscreen_renderer=True,
-        **kwargs
+            self,
+            has_renderer=True,
+            render_collision_mesh=False,
+            render_visual_mesh=True,
+            control_freq=100,
+            horizon=1000,
+            ignore_done=False,
+            use_camera_obs=False,
+            camera_name="frontview",
+            camera_height=256,
+            camera_width=256,
+            camera_depth=False,
+            has_offscreen_renderer=True,
+            **kwargs,
     ):
-        """
-            Initialize a Mujoco Environment
-            @has_renderer: If true, render the simulation state in a viewer instead of headless mode.
-            @control_freq in Hz, how many control signals to receive in every second
-            @ignore_done: if True, never terminate the env
+        """Initialize a Mujoco Environment.
 
-            TODO(extension): What about control_freq = a + bN(0,1) to simulate imperfect timing
+        Args:
+            has_renderer: If true, render the simulation state in a viewer instead of headless mode.
+            render_collision_mesh: True if rendering collision meshes in camera. False otherwise.
+            render_visual_mesh: True if rendering visual meshes in camera. False otherwise.
+            control_freq (in Hz): how many control signals to receive in every second.
+            ignore_done: True if never terminating the environemnt.
+            use_camera_obs: True if rendering camera observations.
+            camera_name: name of camera to be rendered. Must be set if @use_camera_obs is True.
+            camera_height: height of camera frame.
+            camera_width: width of camera frame.
+            camera_depth: True if rendering RGB-D, and RGB otherwise.
+            has_offscreen_renderer: True if using off-screen rendering.
         """
 
-        ### TODO: fix @_load_model if needed ###
-        # self._load_model()
-        # self.mjpy_model = self.model.get_model(mode='mujoco_py')
-        # self.sim = MjSim(self.mjpy_model)
-        # self.initialize_time(control_freq)
-        # self.viewer = MujocoPyRenderer(self.sim)
-        # self.sim_state_initial = self.sim.get_state()
-        # self._get_reference()
-        # self.done = False
-        # self.t = 0
         self.has_renderer = has_renderer
         self.has_offscreen_renderer = has_offscreen_renderer
         self.render_collision_mesh = render_collision_mesh
@@ -77,8 +71,9 @@ class MujocoEnv(object, metaclass=EnvMeta):
         self.horizon = horizon
         self.ignore_done = ignore_done
         self.viewer = None
+        self.model = None
 
-        # settings for camera observation
+        # settings for camera observations
         self.use_camera_obs = use_camera_obs
         self.camera_name = camera_name
         if self.use_camera_obs and self.camera_name is None:
@@ -89,13 +84,8 @@ class MujocoEnv(object, metaclass=EnvMeta):
 
         self._reset_internal()
 
-        # for key in kwargs:
-        #     print('Warning: Parameter {} not recognized'.format(key))
-
     def initialize_time(self, control_freq):
-        """
-            Initialize the time constants used for simulation
-        """
+        """Initialize the time constants used for simulation."""
         self.cur_time = 0
         self.model_timestep = self.sim.model.opt.timestep
         if self.model_timestep <= 0:
@@ -105,17 +95,19 @@ class MujocoEnv(object, metaclass=EnvMeta):
             raise SimulationError(
                 "control frequency {} is invalid".format(control_freq)
             )
-        self.control_timestep = 1 / control_freq
+        self.control_timestep = 1. / control_freq
 
     def _load_model(self):
         """Loads an xml model, puts it in self.model"""
-        self.model = None
+        pass
 
     def _get_reference(self):
-        """Set up necessary reference for objects"""
+        """Set up necessary reference for objects."""
         pass
 
     def reset(self):
+        """Reset simulation."""
+        # TODO(yukez): investigate black screen of death
         # if there is an active viewer window, destroy it
         self.close()
         self._reset_internal()
@@ -123,11 +115,14 @@ class MujocoEnv(object, metaclass=EnvMeta):
         return self._get_observation()
 
     def _reset_internal(self):
-        # self.sim.set_state(self.sim_state_initial)
+        """Reset simulation internal configurations."""
+        # instantiate simulation from MJCF model
         self._load_model()
         self.mjpy_model = self.model.get_model(mode="mujoco_py")
         self.sim = MjSim(self.mjpy_model)
         self.initialize_time(self.control_freq)
+
+        # create visualization screen or renderer
         if self.has_renderer and self.viewer is None:
             self.viewer = MujocoPyRenderer(self.sim)
             self.viewer.viewer.vopt.geomgroup[0] = (
@@ -145,65 +140,76 @@ class MujocoEnv(object, metaclass=EnvMeta):
                 1 if self.render_visual_mesh else 0
             )
 
+        # additional housekeeping
         self.sim_state_initial = self.sim.get_state()
         self._get_reference()
         self.cur_time = 0
-        self.t = 0
+        self.timestep = 0
         self.done = False
 
     def _get_observation(self):
-        """
-            Returns an OrderedDict containing observations [(name_string, np.array), ...]
-        """
+        """Returns an OrderedDict containing observations [(name_string, np.array), ...]."""
         return OrderedDict()
 
     def step(self, action):
-        reward = 0
-        info = None
-        if not self.done:
-            self.t += 1
-            self._pre_action(action)
-            end_time = self.cur_time + self.control_timestep
-            while self.cur_time < end_time:
-                self.sim.step()
-                self.cur_time += self.model_timestep
-            reward, done, info = self._post_action(action)
-            return self._get_observation(), reward, done, info
-        else:
+        """Take a step in simulation with control command @action."""
+        if self.done:
             raise ValueError("executing action in terminated episode")
 
+        self.timestep += 1
+        self._pre_action(action)
+        end_time = self.cur_time + self.control_timestep
+        while self.cur_time < end_time:
+            self.sim.step()
+            self.cur_time += self.model_timestep
+        reward, done, info = self._post_action(action)
+        return self._get_observation(), reward, done, info
+
     def _pre_action(self, action):
+        """Do any preprocessing before taking an action."""
         self.sim.data.ctrl[:] = action
 
     def _post_action(self, action):
+        """Do any housekeeping after taking an action."""
         reward = self.reward(action)
-        self.done = (self._check_terminated() or self.t >= self.horizon) and (
-            not self.ignore_done
-        )
-        # TODO: how to manage info?
+        terminated = self._check_terminated() or self.timestep >= self.horizon
+        self.done = terminated and not self.ignore_done
         return reward, self.done, {}
 
     def reward(self, action):
+        """Reward should be a function of state and action."""
         return 0
 
     def render(self, camera_id=0):
+        """Render image observation from camera @camera_id"""
         self.viewer.render(camera_id=camera_id)
 
     def observation_spec(self):
+        """Returns an observation as observation specification.
+
+        An alternative design is to return an OrderedDict where the keys
+        are the observation names and the values are the shapes of observations.
+        We leave this alternative implementation commented out, as we find the
+        current design is easier to use in practice.
+        """
         observation = self._get_observation()
         return observation
+
         # observation_spec = OrderedDict()
         # for k, v in observation.items():
         #     observation_spec[k] = v.shape
         # return observation_spec
 
     def action_spec(self):
+        """Action specification should be implemented in subclasses.
+
+        Action space is represented by a tuple of (low, high), which are two numpy
+        vectors that specify the min/max action limits per dimension.
+        """
         raise NotImplementedError
 
     def reset_from_xml_string(self, xml_string):
-        """
-        Reloads the environment from an XML description of the environment.
-        """
+        """Reloads the environment from an XML description of the environment."""
 
         # if there is an active viewer window, destroy it
         self.close()
@@ -228,30 +234,39 @@ class MujocoEnv(object, metaclass=EnvMeta):
         self.sim_state_initial = self.sim.get_state()
         self._get_reference()
         self.cur_time = 0
-        self.t = 0
+        self.timestep = 0
         self.done = False
 
     def find_contacts(self, geoms_1, geoms_2):
+        """Finding contact between two geom groups.
+
+        Args:
+            geoms_1: a list of geom names (string)
+            geoms_2: another list of geom names (string)
+
+        Returns:
+            iterator of all contacts between @geoms_1 and @geoms_2
+        """
         for contact in self.sim.data.contact[0 : self.sim.data.ncon]:
-            if (
-                self.sim.model.geom_id2name(contact.geom1) in geoms_1
-                and self.sim.model.geom_id2name(contact.geom2) in geoms_2
-            ) or (
-                self.sim.model.geom_id2name(contact.geom2) in geoms_1
-                and self.sim.model.geom_id2name(contact.geom1) in geoms_2
-            ):
+            # check contact geom in geoms
+            c1_in_g1 = self.sim.model.geom_id2name(contact.geom1) in geoms_1
+            c2_in_g2 = self.sim.model.geom_id2name(contact.geom2) in geoms_2
+            # check contact geom in geoms (flipped)
+            c2_in_g1 = self.sim.model.geom_id2name(contact.geom2) in geoms_1
+            c1_in_g2 = self.sim.model.geom_id2name(contact.geom1) in geoms_2
+            if (c1_in_g1 and c2_in_g2) or (c1_in_g2 and c2_in_g1):
                 yield contact
 
     def _check_contact(self):
-        """
-        Returns True if gripper is in contact with an object.
-        """
+        """Returns True if gripper is in contact with an object."""
+        return False
+
+    def _check_terminated(self):
+        """Returns True if episode is terminated."""
         return False
 
     def close(self):
-        """
-        Do any cleanup necessary here.
-        """
+        """Do any cleanup necessary here."""
         # if there is an active viewer window, destroy it
         if self.viewer is not None:
             self.viewer.close()  # change this to viewer.finish()?
